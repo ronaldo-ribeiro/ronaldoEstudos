6862ad7c486323342d8815806dfb1eccc46c8522
Ronaldo Ribeiro <ronaldo_ribeiro@outlook.com>
2022-10-03 17:09:22 -0300

    Adicionando o primeiro projeto no Git

---
+++ primeiroAppGit/primeiroAppGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  primeiroAppGit
+//
+//  Created by Ronaldo Ribeiro on 02/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
