440785dbcebecdf3ab49178ac162b41b4425b795
Ronaldo Ribeiro <ronaldo_ribeiro@outlook.com>
2022-10-06 17:02:27 -0300

    criei o layout e configuracoes iniciais para os textField

--- PrimeiroDesafioTextFieldDelegate/PrimeiroDesafioTextFieldDelegate/ViewController.swift
+++ PrimeiroDesafioTextFieldDelegate/PrimeiroDesafioTextFieldDelegate/ViewController.swift
@@ -9,11 +9,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var adressTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var registerButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configFontAndColors()
     }
 
+    func configFontAndColors() {
+        view.backgroundColor = .orange
+        nameTextField.placeholder = "Digite seu nome:"
+        adressTextField.placeholder = "Digite seu endereço:"
+        passwordTextField.placeholder = "Digite sua senha:"
+        
+        nameTextField.delegate = self
+        adressTextField.delegate = self
+        passwordTextField.delegate = self
+        
+        
+        
+        nameTextField.layer.borderWidth = 2
+        nameTextField.layer.borderColor = UIColor.lightGray.cgColor
+        
+        adressTextField.layer.borderWidth = 2
+        adressTextField.layer.borderColor = UIColor.lightGray.cgColor
+        
+        passwordTextField.layer.borderWidth = 2
+        passwordTextField.layer.borderColor = UIColor.lightGray.cgColor
+        
+        registerButton.setTitle("Cadastrar", for: .normal)
+        registerButton.backgroundColor = .blue
+        registerButton.setTitleColor(.white, for: .normal)
+    }
 
+    @IBAction func tappedRegisterButton(_ sender: UIButton) {
+        
+    }
+     
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.layer.borderColor = UIColor.systemBlue.cgColor
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField.text == "" {
+            textField.layer.borderColor = UIColor.red.cgColor
+        } else {
+            textField.layer.borderColor = UIColor.lightGray.cgColor
+        }
+            
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
