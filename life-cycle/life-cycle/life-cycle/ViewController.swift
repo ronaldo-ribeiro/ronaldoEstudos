b406649572ccce90b329067eca077c9f61d13689
Ronaldo Ribeiro <ronaldo_ribeiro@outlook.com>
2022-10-11 20:46:23 -0300

    criação de projeto de life cycle

---
+++ life-cycle/life-cycle/life-cycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  life-cycle
+//
+//  Created by Ronaldo Ribeiro on 11/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
