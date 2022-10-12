a0d971b5dd8d8f67b282a3d33ae6155eb0ca51e0
Ronaldo Ribeiro <ronaldo_ribeiro@outlook.com>
2022-10-11 21:01:54 -0300

    criação dos life cycles

--- life-cycle/life-cycle/life-cycle/ViewController.swift
+++ life-cycle/life-cycle/life-cycle/ViewController.swift
@@ -11,7 +11,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(#function)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print(#function)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print(#function)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print(#function)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print(#function)
     }
 
 
