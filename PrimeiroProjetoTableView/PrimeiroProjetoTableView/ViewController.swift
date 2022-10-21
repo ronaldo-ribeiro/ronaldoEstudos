08db59922ed02c52c462009d02ca06e0dd36630d
Ronaldo Ribeiro <ronaldo_ribeiro@outlook.com>
2022-10-20 21:02:02 -0300

    Criação do projeto tableView

---
+++ PrimeiroProjetoTableView/PrimeiroProjetoTableView/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PrimeiroProjetoTableView
+//
+//  Created by Ronaldo Ribeiro on 20/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
