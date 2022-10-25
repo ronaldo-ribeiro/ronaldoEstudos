09b3b08fdfa3e8d2aeb4b33d34cc0703742fabae
Ronaldo Ribeiro <ronaldo_ribeiro@outlook.com>
2022-10-25 20:08:34 -0300

    Criação do projeto DesafioTableView5Marcas da aula de 25/10/2022

---
+++ DesafioTableView5Marcas/DesafioTableView5Marcas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DesafioTableView5Marcas
+//
+//  Created by Ronaldo Ribeiro on 25/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
